5cba5245c12fa86927419b1e24287aa369d1a3ca
Joshua Blevins <josh.adam.blevins@gmail.com>
2022-07-05 18:32:53 -0700

    feat: code is fully tested

--- src/components/Code/Code.test.tsx
+++ src/components/Code/Code.test.tsx
@@ -3,6 +3,7 @@
  */
  import * as React from 'react';
  import { render, screen } from '@testing-library/react';
+ import userEvent from '@testing-library/user-event';
  
  import Code from './Code';
 
@@ -19,5 +20,24 @@
         </Code>);
 
         expect(screen.getByRole('code')).toBeInTheDocument();
+    });
+
+    it('should copy the code contents to clipboard', async () => {
+        const user = userEvent.setup();
+        const copySpy = jest.spyOn(navigator.clipboard, 'writeText');
+
+        render(<Code
+            copyIcon={null}
+            id='ts'
+            language='Typescript'
+        >
+            {`
+                code
+            `}
+        </Code>);
+
+        await user.click(await screen.findByRole('button'));
+
+        expect(copySpy).toHaveBeenCalledTimes(1);
     });
  });
